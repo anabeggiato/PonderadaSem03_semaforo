90d041a103cb994eece8f1543d6234da747c48bc
anabeggiato <158532703+anabeggiato@users.noreply.github.com>
2024-10-31 15:00:26 -0300

    feat: implementa lógica do buzzer

--- semaforo.ino
+++ semaforo.ino
@@ -2,6 +2,7 @@
 int8_t led_vermelho = 14;
 int8_t led_amarelo = 12;
 int8_t led_verde = 13;
+int8_t buzzer = 4;
 
 // definindo os intervalos (em milissegundos)
 int16_t tempo_vermelho = 6000;
@@ -13,7 +14,7 @@ void setup() {
   pinMode(led_vermelho, OUTPUT);
   pinMode(led_amarelo, OUTPUT);
   pinMode(led_verde, OUTPUT);
-
+  pinMode(buzzer, OUTPUT);
 };
 
 
@@ -36,5 +37,20 @@ void loop() {
   // 2 segundos no amarelo
   digitalWrite(led_amarelo, HIGH);
   delay(tempo_amarelo);
+
+  //soar um alarme  e piscar o vermelho pra avisar que vai fechar
+  tone(buzzer, 5000);
+  delay(1000);
+  noTone(buzzer);
+  digitalWrite(led_vermelho, HIGH);
+  delay(200);
+  digitalWrite(led_vermelho, LOW);
+  delay(200);
+  digitalWrite(led_vermelho, HIGH);
+  delay(500);
+  digitalWrite(led_vermelho, LOW);
+  delay(500);
+
+  //desligar o amarelo
   digitalWrite(led_amarelo, LOW);
 };
